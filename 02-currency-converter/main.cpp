40ef9932220a35a2d6cc4a4254e51afd5cfae7da
trybick <t.rybicki@gmail.com>
2024-01-08 12:24:06 -0500

    Basic currency converter

--- 02-currency-converter/main.cpp
+++ 02-currency-converter/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+
 
 int main() {
-    int num;
-    int answer = 0;
+    double USD_TO_EUR = 0.91;
+    double EUR_TO_USD = 1.09;
+    int option;
+    int amount;
 
-    std::cout << "Type a number to get the factorial: ";
-    std::cin >> num;
+    std::cout << "1. Convert USD to EUR" << std::endl;
+    std::cout << "2. Option EUR to USD" << std::endl;
+    std::cout << "Select an option: ";
+    std::cin >> option;
 
-    for (int i = 1; i <= num; i++) {
-      answer += i;
+    if (option != 1 && option != 2) {
+        throw std::invalid_argument("Invalid input");
+        return 1;
     }
 
-    std::cout << ("The answer is " + std::to_string(answer));
+    std::cout << "Enter an amount: ";
+    std::cin >> amount;
+
+    if (option == 1) {
+        float answer = amount * USD_TO_EUR;
+        std::stringstream stream;
+        stream << std::fixed << std::setprecision(2) << answer;
+        std::string s = stream.str();
+        std::cout << (std::to_string(amount) + " USD is " + s) + " EUR";
+    } else if (option == 2) {
+        float answer = amount * EUR_TO_USD;
+        std::stringstream stream;
+        stream << std::fixed << std::setprecision(2) << answer;
+        std::string s = stream.str();
+        std::cout << (std::to_string(amount) + " EUR is " + s) + " USD";
+    }
 
     return 0;
 }
