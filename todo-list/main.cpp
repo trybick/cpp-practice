b02fe03ab6ed5fbaf8b1de7d6aeda357800d63cb
trybick <t.rybicki@gmail.com>
2024-01-14 15:22:48 -0500

    Create show header and show todos base

--- todo-list/main.cpp
+++ todo-list/main.cpp
@@ -5,6 +5,23 @@
 // - add new todo
 // - mark a todo as complete
 
-int main() {
+void showHeader() {
+    std::cout << "  _____           _       " << std::endl;
+    std::cout << " |_   _|__     __| | ___  " << std::endl;
+    std::cout << "   | |/ _ \\   / _` |/ _ \\ " << std::endl;
+    std::cout << "   | | (_) | | (_| | (_) |" << std::endl;
+    std::cout << "   |_|\\___/   \\__,_|\\___/ " << std::endl;
+}
 
+void showTodos() {
+  std::cout << "\n";
+  std::cout << "Todos:";
+  // fetch todos
+}
+
+int main() {
+  showHeader();
+  showTodos();
+
+	return 0;
 }
