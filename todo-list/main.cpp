316bdb32fa43e4096e656632b7a8ca476a5a3896
trybick <t.rybicki@gmail.com>
2024-01-16 12:44:03 -0500

    Format

--- todo-list/main.cpp
+++ todo-list/main.cpp
@@ -5,11 +5,11 @@
 // - mark a todo as complete
 
 void showHeader() {
-    std::cout << "  _____           _       " << std::endl;
-    std::cout << " |_   _|__     __| | ___  " << std::endl;
-    std::cout << "   | |/ _ \\   / _` |/ _ \\ " << std::endl;
-    std::cout << "   | | (_) | | (_| | (_) |" << std::endl;
-    std::cout << "   |_|\\___/   \\__,_|\\___/ " << std::endl;
+  std::cout << "  _____           _       " << std::endl;
+  std::cout << " |_   _|__     __| | ___  " << std::endl;
+  std::cout << "   | |/ _ \\   / _` |/ _ \\ " << std::endl;
+  std::cout << "   | | (_) | | (_| | (_) |" << std::endl;
+  std::cout << "   |_|\\___/   \\__,_|\\___/ " << std::endl;
 }
 
 void showTodos() {
@@ -24,35 +24,35 @@ int main() {
 
   int choice;
   while (shouldDisplayMainMenu) {
-      std::cout << "Select an option:" << std::endl;
-      std::cout << "1. Add a task" << std::endl;
-      std::cout << "2. Edit a task" << std::endl;
-      std::cout << "0. Exit" << std::endl;
+    std::cout << "Select an option:" << std::endl;
+    std::cout << "1. Add a task" << std::endl;
+    std::cout << "2. Edit a task" << std::endl;
+    std::cout << "0. Exit" << std::endl;
 
-      std::cout << "Enter your choice: ";
-      std::cin >> choice;
+    std::cout << "Enter your choice: ";
+    std::cin >> choice;
 
-      switch (choice) {
-          case 1:
-              shouldDisplayMainMenu = false;
+    switch (choice) {
+      case 1:
+        shouldDisplayMainMenu = false;
 
-              // add a todo
+        // add a todo
+        std::cout << "Select an option:" << std::endl;
 
-
-              break;
-          case 2:
-              std::cout << "You selected Option 2." << std::endl;
-              shouldDisplayMainMenu = false;
-              break;
-          case 0:
-              std::cout << "Exiting the program." << std::endl;
-              break;
-          default:
-              std::cout << "Invalid choice. Please enter a valid option." << std::endl;
-              break;
-      }
+        break;
+      case 2:
+        std::cout << "You selected Option 2." << std::endl;
+        shouldDisplayMainMenu = false;
+        break;
+      case 0:
+        std::cout << "Exiting the program." << std::endl;
+        break;
+      default:
+        std::cout << "Invalid choice. Please enter a valid option."
+                  << std::endl;
+        break;
+    }
   };
 
-
-	return 0;
+  return 0;
 }
