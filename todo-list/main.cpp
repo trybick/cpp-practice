09b3bce25747263c9816ff38ea6a1baf7a4548c0
trybick <t.rybicki@gmail.com>
2024-01-16 12:38:50 -0500

    Create choice selection

--- todo-list/main.cpp
+++ todo-list/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-// Run program and show current todos
 // Receive input for and handle:
 // - add new todo
 // - mark a todo as complete
@@ -14,14 +13,46 @@ void showHeader() {
 }
 
 void showTodos() {
-  std::cout << "\n";
-  std::cout << "Todos:";
   // fetch todos
 }
 
 int main() {
   showHeader();
   showTodos();
+
+  bool shouldDisplayMainMenu = true;
+
+  int choice;
+  while (shouldDisplayMainMenu) {
+      std::cout << "Select an option:" << std::endl;
+      std::cout << "1. Add a task" << std::endl;
+      std::cout << "2. Edit a task" << std::endl;
+      std::cout << "0. Exit" << std::endl;
+
+      std::cout << "Enter your choice: ";
+      std::cin >> choice;
+
+      switch (choice) {
+          case 1:
+              shouldDisplayMainMenu = false;
+
+              // add a todo
+
+
+              break;
+          case 2:
+              std::cout << "You selected Option 2." << std::endl;
+              shouldDisplayMainMenu = false;
+              break;
+          case 0:
+              std::cout << "Exiting the program." << std::endl;
+              break;
+          default:
+              std::cout << "Invalid choice. Please enter a valid option." << std::endl;
+              break;
+      }
+  };
+
 
 	return 0;
 }
